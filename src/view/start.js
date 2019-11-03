699301d723df96d37e3766870df7252f537659cc
BlueSky <bluesky@BlueSkys-Mac.local>
2019-11-03 13:29:24 -0800

    updated versionm

--- src/view/start.js
+++ src/view/start.js
@@ -26,7 +26,7 @@ class Start extends Component {
 
         //console.log(' ====== splash screen hide');
         const date1 = new Date();
-        const date2 = new Date('11/10/2019');
+        const date2 = new Date('11/20/2019');
         const diffTime = date2 - date1;
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         if (diffDays <= 0) {
